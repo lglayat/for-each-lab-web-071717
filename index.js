97223e7bbfb1b25c698ad8028955ac584bdd3fdb
lglayat <glayatle@g.cofc.edu>
2017-08-29 11:39:16 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("hey")
+function iterativeLog(array){
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback){
+  const array = [1,2,3,4,5]
+  array.forEach(callback)
+
+  return array
+}
+
+function doToArray(array,callback){
+  array.forEach(callback)
+}
